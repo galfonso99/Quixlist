521517736d85d8360276b79158410f3c35a39f83
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2022-06-12 10:54:25 -0400

    Fix for vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
+// import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel'
+import svg from '@poppanator/sveltekit-svg';
+// import netlify from '@sveltejs/adapter-netlify'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: [
 		preprocess()
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 
+		// hydrate the <div id="svelte"> element in src/app.html
 		vite: {
+			// Options are optional
+			plugins: [svg()],
+			css: { }
 		}
 	}
 };
